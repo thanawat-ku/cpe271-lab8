e3863f25b59b1dfc3f962d21d7d862594802a620
Thanawat Phattaraworamet <thanawat.pha@ku.th>
2025-09-17 11:33:13 +0700

    lab8

---
+++ receive_data/receive_data.ino
@@ -0,0 +1,108 @@
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+#include <PubSubClient.h>
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+// LED Pin
+const int ledPin = 4;
+
+const char *mqtt_server = "YOUR_MQTT_BROKER_IP_ADDRESS";
+
+void setup()
+{
+    WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+    // it is a good practice to make sure your code sets wifi mode how you want it.
+
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+
+    // WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wm;
+
+    // reset settings - wipe stored credentials for testing
+    // these are stored by the esp library
+    // wm.resetSettings();
+
+    // Automatically connect using saved credentials,
+    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
+    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
+    // then goes into a blocking loop awaiting configuration and will return success result
+
+    bool res;
+    // res = wm.autoConnect(); // auto generated AP name from chipid
+    // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
+    res = wm.autoConnect("AutoConnectAP", "password"); // password protected ap
+
+    if (!res)
+    {
+        Serial.println("Failed to connect");
+        // ESP.restart();
+    }
+    else
+    {
+        // if you get here you have connected to the WiFi
+        Serial.println("connected...yeey :)");
+    }
+    client.setServer(mqtt_server, 1883);
+    client.setCallback(callback);
+    pinMode(ledPin, OUTPUT);
+}
+
+void callback(char *topic, byte *message, unsigned int length)
+{
+    if (String(topic) == "esp32-xxxx/output")
+    {
+        Serial.print("Changing output to ");
+        if (messageTemp == "on")
+        {
+            Serial.println("on");
+            digitalWrite(ledPin, HIGH);
+        }
+        else if (messageTemp == "off")
+        {
+            Serial.println("off");
+            digitalWrite(ledPin, LOW);
+        }
+    }
+}
+
+void reconnect()
+{
+    // Loop until we're reconnected
+    while (!client.connected())
+    {
+        Serial.print("Attempting MQTT connection...");
+        // Attempt to connect
+        if (client.connect("ESP8266Client"))
+        {
+            Serial.println("connected");
+            client.subscribe("esp32-xxxx/output");
+        }
+        else
+        {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            // Wait 5 seconds before retrying
+            delay(5000);
+        }
+    }
+}
+
+void loop()
+{
+    if (!client.connected())
+    {
+        reconnect();
+    }
+    client.loop();
+
+    // put your main code here, to run repeatedly:
+    long now = millis();
+    if (now - lastMsg > 5000)
+    {
+        lastMsg = now;
+        client.publish("esp32-xxxx/msg", tempString);
+    }
+}
