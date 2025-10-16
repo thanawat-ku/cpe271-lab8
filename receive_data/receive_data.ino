7d81f37cc3f4e8a8688dd110b223569dd1d1eb92
Fook <59011305+thanawat-ku@users.noreply.github.com>
2025-10-16 15:31:46 +0700

    Update receive_data.ino

--- receive_data/receive_data.ino
+++ receive_data/receive_data.ino
@@ -1,4 +1,4 @@
-#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+#include <WiFiManager.h>  // https://github.com/tzapu/WiFiManager
 #include <PubSubClient.h>
 
 WiFiClient espClient;
@@ -9,100 +9,88 @@ const int ledPin = 2;
 
 const char *mqtt_server = "mqtt-dashboard.com";
 
-void setup()
-{
-    WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
-    // it is a good practice to make sure your code sets wifi mode how you want it.
+void setup() {
+  WiFi.mode(WIFI_STA);  // explicitly set mode, esp defaults to STA+AP
+  // it is a good practice to make sure your code sets wifi mode how you want it.
 
-    // put your setup code here, to run once:
-    Serial.begin(115200);
+  // put your setup code here, to run once:
+  Serial.begin(115200);
 
-    // WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
-    WiFiManager wm;
+  // WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wm;
 
-    // reset settings - wipe stored credentials for testing
-    // these are stored by the esp library
-    // wm.resetSettings();
+  // reset settings - wipe stored credentials for testing
+  // these are stored by the esp library
+  // wm.resetSettings();
 
-    // Automatically connect using saved credentials,
-    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
-    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
-    // then goes into a blocking loop awaiting configuration and will return success result
+  // Automatically connect using saved credentials,
+  // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
+  // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
+  // then goes into a blocking loop awaiting configuration and will return success result
 
-    bool res;
-    // res = wm.autoConnect(); // auto generated AP name from chipid
-    // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
-    res = wm.autoConnect("AutoConnectAP", "password"); // password protected ap
+  bool res;
+  // res = wm.autoConnect(); // auto generated AP name from chipid
+  // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
+  res = wm.autoConnect("AutoConnectAP", "password");  // password protected ap
 
-    if (!res)
-    {
-        Serial.println("Failed to connect");
-        // ESP.restart();
-    }
-    else
-    {
-        // if you get here you have connected to the WiFi
-        Serial.println("connected...yeey :)");
-    }
-    client.setServer(mqtt_server, 1883);
-    client.setCallback(callback);
-    pinMode(ledPin, OUTPUT);
+  if (!res) {
+    Serial.println("Failed to connect");
+    // ESP.restart();
+  } else {
+    // if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
+  }
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  pinMode(ledPin, OUTPUT);
 }
 
-void callback(char *topic, byte *message, unsigned int length)
-{
-    if (String(topic) == "esp32-1111/output")
-    {
-        Serial.print("Changing output to ");
-        if (messageTemp == "on")
-        {
-            Serial.println("on");
-            digitalWrite(ledPin, HIGH);
-        }
-        else if (messageTemp == "off")
-        {
-            Serial.println("off");
-            digitalWrite(ledPin, LOW);
-        }
+void callback(char *topic, byte *message, unsigned int length) {
+  if (String(topic) == "esp32-1111/output") {
+    String messageTemp;
+    for (int i = 0; i < length; i++) {
+      Serial.print((char)message[i]);
+      messageTemp += (char)message[i];
     }
+    Serial.print("Changing output to ");
+    if (messageTemp == "on") {
+      Serial.println("on");
+      digitalWrite(ledPin, HIGH);
+    } else if (messageTemp == "off") {
+      Serial.println("off");
+      digitalWrite(ledPin, LOW);
+    }
+  }
 }
 
-void reconnect()
-{
-    // Loop until we're reconnected
-    while (!client.connected())
-    {
-        Serial.print("Attempting MQTT connection...");
-        // Attempt to connect
-        if (client.connect("ESP8266Client"))
-        {
-            Serial.println("connected");
-            client.subscribe("esp32-1111/output");
-        }
-        else
-        {
-            Serial.print("failed, rc=");
-            Serial.print(client.state());
-            Serial.println(" try again in 5 seconds");
-            // Wait 5 seconds before retrying
-            delay(5000);
-        }
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      client.subscribe("esp32-1111/output");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
     }
+  }
 }
 
-void loop()
-{
-    if (!client.connected())
-    {
-        reconnect();
-    }
-    client.loop();
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
 
-    // put your main code here, to run repeatedly:
-    long now = millis();
-    if (now - lastMsg > 5000)
-    {
-        lastMsg = now;
-        client.publish("esp32-1111/msg", tempString);
-    }
+  // put your main code here, to run repeatedly:
+  long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+    client.publish("esp32-1111/msg", tempString);
+  }
 }
